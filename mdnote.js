02b4b3851c86db19d56d54f10a9417df8847f94f
Robert Rose <rose@perrito2.attlocal.net>
2016-01-03 21:15:44 -0800

    Refactored gmail interface in GmailInterface object

--- mdnote.js
+++ mdnote.js
@@ -7,6 +7,148 @@ function onGapiLoaded() {
 var CLIENT_ID = '468212145523-8qedsjk185kkrobrstgtroqqs6oufjbl.apps.googleusercontent.com';
 var SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
 
+var GmailInterface = {
+    authorize: function(callback) {
+        // invoke gmail authorization function
+        gapi.auth.authorize({
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+        }, function(authResult) {
+            console.log(authResult);
+            if (authResult && !authResult.error) {
+                // load gmail client interface, then trigger callback
+                gapi.client.load('gmail', 'v1', function() {
+                    callback(true);
+                });
+            } else {
+                // authorization failed
+                callback(false);
+            }
+        });
+    },
+
+    getNotesLabelId: function(callback) {
+        if (GmailInterface.notesLabelId === undefined) {
+            GmailInterface.loadNotesLabelId(callback);
+        } else {
+            callback(GmailInterface.notesLabelId);
+        }
+    },
+
+    loadNotesLabelId: function(callback) {
+        var request = gapi.client.gmail.users.labels.list({
+            'userId': 'me'
+        });
+
+        request.execute(function(resp) {
+            for (i = 0; i < resp.labels.length; i++) {
+                var label = resp.labels[i];
+
+                if (label.name == "Notes") {
+                    GmailInterface.notesLabelId = label.id;
+                    break;
+                }
+            }
+
+            callback(GmailInterface.notesLabelId);
+        });
+    },
+ 
+    // Get all drafts from Gmail
+    getDrafts: function(callback) {
+        var request = gapi.client.gmail.users.drafts.list({
+            'userId': 'me'
+        });
+
+        request.execute(function(resp) {
+            callback(resp.drafts);
+        });
+    },
+
+    // Get the labelIds, date and snippet for a Gmail message
+    getDraftSnippet: function(draftId, messageId, callback) {
+        var request = gapi.client.gmail.users.messages.get({
+            'userId': 'me',
+            'id': messageId,
+            'fields': 'id,internalDate,labelIds,snippet'
+        });
+
+        request.execute(function(resp) {
+            callback(draftId, messageId, resp.internalDate, resp.labelIds, resp.snippet);
+        });
+    },
+
+    // Get all the note snippets, fires a callback for each note
+    getNoteSnippets: function(callback) {
+        // Get the labelId for Notes
+        GmailInterface.getNotesLabelId(function(notesLabelId) {
+
+            var noteSnippetCallback = function(draftId, messageId, internalDate, labelIds, snippet) {
+                // Fire the callback if this draft is labeled with the 'notes' labelId
+                if ($.inArray(notesLabelId, labelIds) >= 0) {
+                    callback(draftId, messageId, internalDate, snippet);
+                }
+            };
+
+            // Get the list of drafts
+            GmailInterface.getDrafts(function(drafts) {
+                for (i = 0; i < drafts.length; i++) {
+                    // Get the labelIds and snippet for this draft
+                    GmailInterface.getDraftSnippet(drafts[i].id, drafts[i].message.id, noteSnippetCallback);
+                }
+            });
+        });
+    },
+
+    // Retrieves the message body as plain text from the draft response
+    getDraftBody: function(messagePayload) {
+        var body = {};
+
+        if (messagePayload.mimeType == "multipart/alternative") {
+            for (i = 0; i < messagePayload.parts.length; i++) {
+                if (messagePayload.parts[i].mimeType == "text/plain") {
+                    body = messagePayload.parts[i].body;
+                }
+            }
+        } else {
+            body = messagePayload.body;
+        }
+
+        return atob(body.data);
+    },
+
+    // Retrieves the draft content from Gmail
+    getDraft: function(draftId, callback) {
+        var request = gapi.client.gmail.users.drafts.get({
+            'userId': 'me',
+            'id': draftId
+        });
+
+        request.execute(function(resp) {
+            var draft_decoded = GmailInterface.getDraftBody(resp.message.payload);
+            callback(draft_decoded);
+        });
+    },
+
+    updateDraft: function(draftId, content) {
+        var draft_encoded = btoa("Subject: \r\n\r\n" + content);
+        var request = gapi.client.gmail.users.drafts.update({
+            'userId': 'me',
+            'id': draftId,
+                'message': {
+                    'raw': draft_encoded
+                
+            },
+            'send': false
+        });
+
+        request.execute(function(resp) {
+            console.log(resp);
+        });
+    }
+};
+
 var MyApp = angular.module('MyApp', ['ui.router', 'ui.bootstrap.buttons', 'moof2k.wysimd']);
 
 MyApp.config(function($stateProvider, $urlRouterProvider) {
@@ -57,31 +199,15 @@ MyApp.config(function($stateProvider, $urlRouterProvider) {
 
 MyApp.controller('AuthController', function($scope, $state, $window) {
     $scope.authorized = false;
+    $scope.notesLabelId = undefined;
 
-    function setAuthorized(a) {
+    function isAuthorized(a) {
         $scope.authorized = a;
         $scope.$apply();
     }
 
-    function checkGapiAuth() {
-        gapi.auth.authorize({
-            'client_id': CLIENT_ID,
-            'scope': SCOPES.join(' '),
-            'immediate': true
-        }, onGapiAuthResult);
-    }
-
-    function onGapiAuthResult(authResult) {
-        console.log(authResult);
-        if (authResult && !authResult.error) {
-            $scope.$evalAsync(setAuthorized(true));
-        } else {
-            $scope.$evalAsync(setAuthorized(false));
-        }
-    }
-
     $window.init = function() {
-        checkGapiAuth();
+        GmailInterface.authorize(isAuthorized);
     };
 
     if (window.location.hash.endsWith("/test")) {
@@ -91,21 +217,20 @@ MyApp.controller('AuthController', function($scope, $state, $window) {
 });
 
 
-MyApp.controller('NotesController', function($scope) {
+MyApp.controller('NotesController', function($scope, $timeout) {
     $scope.notes = [];
-    $scope.note = {
-        'model': "foo",
-        'editable': true,
-        'id': 0
-    };
 
     function addNote(draftId, messageId, messageTimestamp, messageSnippet) {
-        console.log(messageSnippet);
-        $scope.notes.push({
-            'messageId': messageId,
-            'draftId': draftId,
-            'time': new Date(parseInt(messageTimestamp)),
-            'snippet': messageSnippet
+        // This function may be called in response to a data load callback so wrap in a timeout
+        // so the scope can get updated.
+        $timeout(function() {
+            console.log(messageSnippet);
+            $scope.notes.push({
+                'messageId': messageId,
+                'draftId': draftId,
+                'time': new Date(parseInt(messageTimestamp)),
+                'snippet': messageSnippet
+            });
         });
     }
 
@@ -130,69 +255,15 @@ MyApp.controller('NotesController', function($scope) {
         return Math.floor(seconds) + " seconds ago";
     };
 
-    function getNotesLabelId() {
-        var request = gapi.client.gmail.users.labels.list({
-            'userId': 'me'
-        });
-
-        request.execute(function(resp) {
-            for (i = 0; i < resp.labels.length; i++) {
-                var label = resp.labels[i];
-
-                if (label.name == "Notes") {
-                    $scope.notesLabelId = label.id;
-                    console.log("notes label is " + label.id);
-                    getDrafts();
-                }
-            }
-        });
-    }
-
-    function getDrafts() {
-        var request = gapi.client.gmail.users.drafts.list({
-            'userId': 'me'
-        });
-
-        request.execute(function(resp) {
-            for (i = 0; i < resp.drafts.length; i++) {
-                var draft = resp.drafts[i];
-
-                getDraftSnippet(draft.id, draft.message.id);
-            }
-        });
-    }
-
-    function getDraftSnippet(draftId, messageId) {
-        console.log(draftId + " " + messageId);
-        var request = gapi.client.gmail.users.messages.get({
-            'userId': 'me',
-            'id': messageId,
-            'fields': 'id,internalDate,labelIds,snippet'
-        });
-
-        request.execute(function(resp) {
-            if ($.inArray($scope.notesLabelId, resp.labelIds) >= 0) {
-                $scope.$evalAsync(addNote(draftId, messageId, resp.internalDate, resp.snippet));
-            }
-        });
-    }
-
-    function onGmailLoaded() {
-        console.log('onGmailLoaded Notes');
-        getNotesLabelId();
-    }
-
-    console.log('NotesController');
-
     if (!window.location.hash.endsWith("/test")) {
-        gapi.client.load('gmail', 'v1', onGmailLoaded);
+        GmailInterface.getNoteSnippets(addNote);
     } else {
         addNote('test', 'test', 0, 'test note');
     }
 });
 
 
-MyApp.controller('NoteController', function($scope, $stateParams) {
+MyApp.controller('NoteController', function($scope, $stateParams, $timeout) {
     $scope.note = {
         'model': "foo",
         'editable': true,
@@ -203,64 +274,22 @@ MyApp.controller('NoteController', function($scope, $stateParams) {
     $scope.editmode = 'right';
     $scope.note_data = "";
 
-    // Retrieves the message body as plain text from the draft response
-    function getNoteBody(messagePayload) {
-        var body = {};
-
-        if (messagePayload.mimeType == "multipart/alternative") {
-            for (i = 0; i < messagePayload.parts.length; i++) {
-                if (messagePayload.parts[i].mimeType == "text/plain") {
-                    body = messagePayload.parts[i].body;
-                }
-            }
-        } else {
-            body = messagePayload.body;
-        }
-
-        return atob(body.data);
-    }
-
-    function getDraft() {
-        var request = gapi.client.gmail.users.drafts.get({
-            'userId': 'me',
-            'id': $stateParams.draftId
-        });
-
-        request.execute(function(resp) {
-            console.log(resp);
-            $scope.note_data = getNoteBody(resp.message.payload);
+    function setNote(noteData) {
+        $timeout(function() {
+            $scope.note_data = noteData;
         });
     }
 
     $scope.updateDraft = function() {
-        var data = btoa("Subject: \r\n\r\n" + $scope.note_data);
-        console.log(data);
-        var request = gapi.client.gmail.users.drafts.update({
-            'userId': 'me',
-            'id': $stateParams.draftId,
-                'message': {
-                    'raw': data
-                
-            },
-            'send': false
-        });
-
-        request.execute(function(resp) {
-            console.log(resp);
-        });
+        GmailInterface.updateDraft($stateParams.draftId, $scope.note_data);
     };
-
-    function onGmailLoaded() {
-        console.log('onGmailLoaded Note');
-        getDraft();
-    }
 
     console.log('NoteController');
 
     if (!window.location.hash.endsWith("/test")) {
-        gapi.client.load('gmail', 'v1', onGmailLoaded);
+        GmailInterface.getDraft($stateParams.draftId, setNote);
     } else {
-        $scope.note_data = "# Test note\n\nHere is a test note";
+        setNote("# Test note\n\nHere is a test note");
     }
 });
 
